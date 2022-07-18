40b100ec95475f058a34323ae61bc882b4e94f2e
steph-GoodLife <stephripa@gmail.com>
2022-07-18 11:09:04 +0200

    màj upstream

--- src/Entity/Specialevents.php
+++ src/Entity/Specialevents.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\SpecialeventsRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SpecialeventsRepository::class)]
