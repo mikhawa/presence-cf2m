db0241d0f2dea3d9d4465968e4f554597bd5cf30
steph-GoodLife <stephripa@gmail.com>
2022-07-25 15:10:38 +0200

    màj

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
-use App\Services\MailerService;
 use LogicException;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Twig\error\LoaderError;
+use Twig\error\SyntaxError;
+use Twig\error\RuntimeError;
+use App\Services\MailerService;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Twig\error\LoaderError;
-use Twig\error\RuntimeError;
-use Twig\error\SyntaxError;
 
 class PublicController extends AbstractController
 {
@@ -102,4 +103,6 @@ class PublicController extends AbstractController
     {
         throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    
 }
