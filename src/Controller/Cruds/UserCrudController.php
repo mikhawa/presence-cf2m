d6a16e4361aa1c3289ea090fcaf60056122b462e
24manu <manuel.mouzelard@hotmail.com>
2022-07-28 14:26:52 +0200

    formulaire user

--- src/Controller/Cruds/UserCrudController.php
+++ src/Controller/Cruds/UserCrudController.php
@@ -4,19 +4,32 @@ namespace App\Controller\Cruds;
 
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserCrudController extends AbstractController
 {
     #[Route('/admin/create/users', name: 'app_admin_create_users')]
-    public function createUsers() : Response
+    public function createUsers(Request $request): Response
     {
-        return $this->render('admin/admin.homepage.html.twig');
+        if ($request->isMethod("POST")) {
+            $tableau = [
+                "username" => $request->request->get("username"),
+                "roles" => [...$request->request]["role"],
+                "password" => $request->request->get("password"),
+                "thename" => $request->request->get("thename"),
+                "thesurname" => $request->request->get("thesurname"),
+                "themail" => $request->request->get("themail"),
+                "thestatus" => $request->request->get("thestatus")
+            ];
+            die(var_dump());
+        }
+        return $this->render('admin/CRUDs/Create/formUser.html.twig');
     }
 
     #[Route('/admin/read/users', name: 'app_admin_read_users')]
-    public function readUsers(UserRepository $repository) : Response
+    public function readUsers(UserRepository $repository): Response
     {
         return $this->render('admin/CRUDs/Read/entities/Users/users.twig', [
             "users" => $repository->findAllUsersByRole(),
@@ -24,19 +37,19 @@ class UserCrudController extends AbstractController
     }
 
     #[Route('/admin/update/user/{username}', name: 'app_admin_update_user')]
-    public function updateUsers(UserRepository $repository, string $username = "") : Response
+    public function updateUsers(UserRepository $repository, string $username = ""): Response
     {
         return $this->render('admin/admin.homepage.html.twig');
     }
 
     #[Route('/admin/delete/users', name: 'app_admin_delete_users')]
-    public function deleteUsers() : Response
+    public function deleteUsers(): Response
     {
         return $this->render('admin/admin.homepage.html.twig');
     }
 
     #[Route('/admin/read/user/{username}', name: 'app_admin_read_user')]
-    public function readUser(UserRepository $repository, string $username = "") : Response
+    public function readUser(UserRepository $repository, string $username = ""): Response
     {
         return $this->render('admin/CRUDs/Read/entities/Users/user.twig', [
             "user" => $repository->findOneByUsername($username),
@@ -44,13 +57,13 @@ class UserCrudController extends AbstractController
     }
 
     #[Route('/admin/ban/user/{username}', name: 'app_admin_ban_user')]
-    public function banUser(UserRepository $repository, string $username = "") : Response
+    public function banUser(UserRepository $repository, string $username = ""): Response
     {
         return $this->render('admin/admin.homepage.html.twig');
     }
 
     #[Route('/admin/update/statut/user/{username}', name: 'app_admin_update_user_statut')]
-    public function updateUserStatut(UserRepository $repository, string $username = "")
+    public function updateUserStatut(UserRepository $repository, string $username = ""): Response
     {
         return $this->render('admin/admin.homepage.html.twig');
     }
