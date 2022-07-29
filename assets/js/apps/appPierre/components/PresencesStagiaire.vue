ebe932437e0bf52a38ca484887614b8a2277ee78
QuentinFayt <quentin.fayt.p@gmail.com>
2022-07-29 16:14:26 +0200

    app Pierre

---
+++ assets/js/apps/appPierre/components/PresencesStagiaire.vue
@@ -0,0 +1,25 @@
+<template>
+  <article class="stagiaire">
+    <identite-stagiaire v-bind:nom="nom" v-bind:prenom="prenom"></identite-stagiaire>
+    <div v-for="i in 5" class="d-flex">
+      <case-horaire signature="P"></case-horaire>
+      <case-horaire signature="P"></case-horaire>
+    </div>
+
+  </article>
+</template>
+
+<script>
+import CaseHoraire from './CaseHoraire.vue'
+import IdentiteStagiaire from './IdentiteStagiaire.vue'
+
+export default {
+  props     : ["nom", "prenom", "events"],
+  components: {IdentiteStagiaire, CaseHoraire}
+}
+
+</script>
+
+<style scoped>
+
+</style>
